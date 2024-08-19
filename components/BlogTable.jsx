443feb9f81b77b4a0a57dbc0d6e60a0673ba8de0
Saketh Rajesh <sakethraj122@gmail.com>
2024-08-19 15:12:22 -0700

    human readable date for blog table

--- components/BlogTable.jsx
+++ components/BlogTable.jsx
@@ -9,6 +9,19 @@ function BlogTable() {
 
     const [blogs, setBlogs] = React.useState([])
 
+    const getHumanReadableDate = (isoDateString) => {
+        const date = new Date(isoDateString);
+
+        // Format the date to show only month, day, and year
+        const humanReadableDate = date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+
+        return humanReadableDate;
+    }
+
     React.useEffect(() => {
         const fetchData = async () => {
             try {
@@ -24,31 +37,31 @@ function BlogTable() {
     }, []);
 
     return (
-      <Skeleton
-        height='75px'
-        isLoaded={ blogs.length > 0 }
-        fadeDuration={1}
-      >
-     
-        <TableContainer mt='5'>
-            <Table variant='simple'>
-                <TableCaption>Just Some Thoughts to Ponder</TableCaption>
-                <Tbody>
-                    {blogs.map((blog) => {
-                        if (blog.publish) {
-                            return (
-                                <Tr onClick={() => router.push(`/blog/${blog.$id}`)} key={blog.id}>
-                                            <Td>{blog.date}</Td>
-                                            <Td>{blog.title}</Td>
-                                            <Td>{blog.tags}</Td>
+        <Skeleton
+            height='75px'
+            isLoaded={blogs.length > 0}
+            fadeDuration={1}
+        >
+
+            <TableContainer mt='5'>
+                <Table variant='simple'>
+                    <TableCaption>Just Some Thoughts to Ponder</TableCaption>
+                    <Tbody>
+                        {blogs.map((blog) => {
+                            if (blog.publish) {
+                                return (
+                                    <Tr onClick={() => router.push(`/blog/${blog.$id}`)} key={blog.id}>
+                                        <Td>{getHumanReadableDate(blog.date)}</Td>
+                                        <Td>{blog.title}</Td>
+                                        {/* <Td>{blog.tags}</Td> */}
                                     </Tr>
-                            )
+                                )
+                            }
+                        })
                         }
-                    })
-                    }
-                </Tbody>
-            </Table>
-        </TableContainer>
+                    </Tbody>
+                </Table>
+            </TableContainer>
         </Skeleton>
     )
 }
