98d00d3705a8518ad3345a9bafb11986a848a3a1
Saketh Rajesh <sakethraj122@gmail.com>
2024-08-18 22:38:50 -0700

    lint changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Divider, Heading } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div>
-      <Heading mt="10" >Hi, I'm <span className="text-primary">Saketh</span>! 👋🏾</Heading>
-      <Divider mt='1' />
+      <Heading mt="10">Hi, I&apos;m Saketh! 👋🏾</Heading>
+      <Divider mt="1" />
     </div>
   );
 }
